d4aa68e12270002fa4e5843813c52af864406792
Songsak C. <songsak.chantorn@gmail.com>
2024-04-04 09:10:13 +0700

    Add project files.

---
+++ Sansiri/wwwroot/js/site.js
@@ -0,0 +1,19 @@
+﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+
+$(function () {
+    $('.slider').slick({
+        slidesToShow: 3,
+        autoplay: true,
+        autoplaySpeed: 2000
+    });
+
+    $('.recommended-project-slider').slick({
+        slidesToShow: 4,
+        dots: true,
+        autoplay: true,
+        autoplaySpeed: 2000
+    });
+});
